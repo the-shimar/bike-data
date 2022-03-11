e3341b3c48d221944c073ce21fb1578014ee474d
Shimar <appotsfirm@gmail.com>
2022-03-11 15:50:54 +0530

    3rd commit

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
-$servername = "sql555.main-hosting.eu";
-$username = "u432291438_bikeshimar";
-$password = "Shimar#Kestone1";
-$dbname = "u432291438_bikegame";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  echo "Hi!!";
 
-$sql = "INSERT INTO bikedata (name, org, des, email)
-VALUES ('John', 'org', 'des', 'john@example.com')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
