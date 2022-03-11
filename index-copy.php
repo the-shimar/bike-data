5373f00a66ef045c294e2dfa077d0d91f11860c9
Shimar <appotsfirm@gmail.com>
2022-03-11 17:51:53 +0530

    form made dynamic2

--- index-copy.php
+++ index-copy.php
@@ -5,10 +5,20 @@ $username = "u432291438_bikeshimar";
 $password = "Shimar#Kestone1";
 $dbname = "u432291438_bikegame";
 
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "bike";
+
 $name = $_POST["name"];
 $org = $_POST["org"];
 $des = $_POST["des"];
 $email = $_POST["email"];
+
+// $name = "name";
+// $org = "org";
+// $des = "des";
+// $email = "email";
 
 
 // Create connection
@@ -18,8 +28,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO bikedata (name, org, des, email)
-VALUES ($name, $org, $des, $email)";
+$sql = "INSERT INTO bikedata (name, org, des, email, score)
+VALUES ($name, $org, $des, $email, 0)";
 
 if ($conn->query($sql) === TRUE) {
   $last_id = $conn->insert_id;
