4270803776b21d54c308be6e8cea20c748a20cb9
Shimar <appotsfirm@gmail.com>
2022-03-14 03:15:37 +0530

    3th commit

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+
 $servername = "sql555.main-hosting.eu";
 $username = "u432291438_bikeshimar";
 $password = "Shimar#Kestone1";
